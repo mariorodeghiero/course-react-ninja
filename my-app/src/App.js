6f40d449bd28d13e34484831033bed523b4f1db4
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-04-18 09:16:53 -0300

    props children

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,17 +3,24 @@ import logo from "./logo.svg";
 import Title from "./components/Title";
 import "./App.css";
 import Square from "./components/square";
+import Button from "./components/button";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className="container">
+        <div
+          className="container"
+          onClick={e => {
+            alert("clicou");
+          }}
+        >
           {["blue", "red", "pink"].map((square, index) => (
             <Square key={index} color={square} />
           ))}
         </div>
         <Title name="Mário Rodeghiero" text="Welcome to..." age={30} />
+        <Button onClick="console.log('tex')">Click</Button>
       </div>
     );
   }
