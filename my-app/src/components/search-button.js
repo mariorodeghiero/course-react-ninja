f282bdd3e9f849d3a31bf4d9b2c4addd38d841f9
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-04-18 21:02:37 -0300

    editado composiçao

--- my-app/src/components/search-button.js
+++ my-app/src/components/search-button.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Button from "./button";
 
-const SearchButton = () => (
-  <Button handleClick={() => alert("search")}>Search</Button>
-);
+const SearchButton = () => <Button handleClick={handleClick}>Search</Button>;
+
+function handleClick() {
+  alert("search");
+}
 
 export default SearchButton;
